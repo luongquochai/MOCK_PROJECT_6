52403a9ff63682a7149c66580ace78c027c808e7
lqhaiii <51089030+luongquochai@users.noreply.github.com>
2019-07-22 10:38:42 +0700

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 signed int counta = 0, countb = 0;
 volatile bool press = true, sent = false;
-volatile unsigned char recvData;
+volatile unsigned char recvData = '0';
 volatile unsigned int done = 0;
 
 void CLK_Init(void);
@@ -23,6 +23,8 @@ int main( void )
 
   LCD_Init((1<<2)|(1<<3)| (1<<4)|(1<<5)|(1<<6)) ;
   asm("RIM\n");
+  while(!(UART->SR & 0x80));
+    UART->DR = 'r';  //goi tin hieu reset
   while (1){
 
     
@@ -36,7 +38,7 @@ void GPIO_Init(void)
   GPIOC->CR1 |= (1<<7);
   GPIOE->DDR |= (1<<7);
   GPIOE->CR1 |= (1<<7);
-  GPIOC->DDR |= (1<<2);
+  GPIOC->DDR &= ~(1<<2);
   GPIOC->CR1 |= (1<<2);
   
   GPIOC->DDR &= ~(1<<1);
@@ -75,54 +77,70 @@ void delay_ms(unsigned int ms)
 #pragma vector = 18
 __interrupt void Display(void)
 {
-   LCD_SEG1(GetHexCodeChar('A'));
-   COL1 = 1;
-   LCD_SEG4(GetHexCodeChar('B'));
-   COL2 = 1;
+     if(recvData == 'r')
+  {
+    counta = 0;
+    countb = 0;
+    UART->CR2 |= (1<<2);
+  }
+//  if(recvData == 'b')
+ // {
+  // countb++;
+  //  recvData = '0';
+ // }
+   if(recvData == 'a')
+ {
+    counta++;
+    recvData = '0';
+  }
+  //check a &b
+  if(counta>=11)
+  {
+    if((counta - countb) >=2 )
+    {
+      LCD->CR3 |=(1<<3); // clear interrupt flag
+      //Display_string("WIN");
+      Display_string("LOSE");
+      done = 1;
+      UART->CR2 &= ~(1<<2);
+    }
+  }
+  if(countb >= 11)
+  {
+    if((countb - counta) >= 2 )
+    {
+      LCD->CR3 |= (1<<3);
+      //Display_string("LOSE");
+      Display_string("WIN");
+      done = 1;
+      UART->CR2 &= ~(1<<2);
+    }
+  }
+  if((counta > 99) && (done == 0 ))
+  {
+    Display_string("WIN");
+    LCD->CR3 |= (1<<3);
+  }
+  if((countb > 99) && (done == 0))
+  {
+    Display_string("WIN");
+    LCD->CR3 |= (1<<3);
+  }
+  if((counta<=99)&&(done==0))   {
    
-   LCD_SEG2(GetHexCodeNum(0));
-   LCD_SEG3(GetHexCodeNum(0));
+   LCD_SEG2(GetHexCodeNum(counta /10));
+   LCD_SEG3(GetHexCodeNum(counta %  10));
    LCD_SEG5(GetHexCodeNum(countb /10));
    LCD_SEG6(GetHexCodeNum(countb %10));
   
-  if(((counta % countb) >= 2) && (counta >=11) )
-  {
-    LCD->CR3 |=(1<<3);
-    
-    Display_string("A WIN");
-  
-    
-    done = 1;
-    
   }
-  if(((countb % counta) >= 2) && (countb >=11))
-  {
-    LCD->CR3 |=(1<<3);
-    UART->CR2 &= ~(1<<2);
-    Display_string("B WIN");
+
 
     
-    UART->CR2 &= ~(1<<2);
-    done = 1;
-  }
-  
-
-  if(done == 0){
-   if(press)
-  {
-     
-     LCD_SEG2(GetHexCodeNum(counta / 10));
-     LCD_SEG3(GetHexCodeNum(counta % 10));
-  }
-  if(sent)
-  {
-    LCD_SEG5(GetHexCodeNum(countb / 10));
-    LCD_SEG6(GetHexCodeNum(countb % 10));
-    sent = false;
-  }
 
   LCD->CR3 |=(1<<3); // clear interrupt flag
-    }
+   
+
 }
 
 #pragma vector = 11
@@ -131,10 +149,11 @@ __interrupt void Press (void)
    
    press = true;
    //counta++;
-   //UART_Transmit('a');
+  // UART_Transmit('a');
 
     countb++;
-    UART_Transmit('b');
+     UART_Transmit('b');
+    
 
    EXTI->SR1 |= (1<<1);
 }
@@ -146,10 +165,10 @@ __interrupt void UART_Recived(void)
   GPIOC->ODR ^= (1<<7);
   sent = true;
   recvData = UART->DR;
-  //if(recvData == 'b')
-    //countb++;
-   if(recvData == 'a')
-     counta++;
+  // //if(recvData == 'b')
+  //   //countb++;
+  //  if(recvData == 'a')
+  //    counta++;
  
 
 }
